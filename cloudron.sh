7bcf8c3f92aee26b5fc3e6fab87262b82c7ed33e
defacube <85292886+yoyeet961@users.noreply.github.com>
2022-08-01 03:08:36 +1100

    Update cloudron.sh

--- cloudron.sh
+++ cloudron.sh
@@ -1,5 +1,14 @@
 sleep 20s
 sudo wget https://cloudron.io/cloudron-setup
 sudo chmod +x ./cloudron-setup
+sudo apt-get purge -y nginx nginx-common
+sudo apt-get purge -y docker docker-engine docker.io docker-ce
+sudo rm -rf /var/lib/docker /etc/docker
+sudo rm /etc/apparmor.d/docker
+sudo groupdel docker
+sudo rm -rf /var/run/docker.sock
+sudo apt purge -y --auto-remove nodejs
 sudo ./cloudron-setup
-sleep 9999999999999999999999999999d
+sleep 30s
+sudo reboot
+sleep 99999999999d
